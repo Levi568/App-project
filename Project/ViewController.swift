e4d911a847fb6b5b86fa614ad83ebab9aba93fe5
Levi <levis830523@gmail.com>
2018-12-06 10:19:48 -0500

    Initial Commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Wei Li on 12/6/18.
+//  Copyright © 2018 Levi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
