24b13b5e296e81abbfb976df9dde449891977c69
Levi <levis830523@gmail.com>
2018-12-20 23:40:15 -0500

    test acceleration threshold

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -24,10 +24,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         // Do any additional setup after loading the view, typically from a nib.
         //mapView.showsUserLocation = true
         //mapView.delegate = self
+        
+        //Authorization for user location
+        if (CLLocationManager.authorizationStatus() == .restricted ||
+            CLLocationManager.authorizationStatus() == .denied ||
+            CLLocationManager.authorizationStatus() == .notDetermined){
+            locationmanager.requestWhenInUseAuthorization()
+        }
         locationmanager.delegate = self
         locationmanager.desiredAccuracy = kCLLocationAccuracyBest   //Accuracy
-        locationmanager.requestWhenInUseAuthorization()             //Authorization for user location
         locationmanager.startUpdatingLocation()                     //Update the location
+        
+        myAccelerometer()
         
     }
     
@@ -46,28 +54,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     //Accelerometer
-    let motionManager = CMMotionManager()
+    var motionManager = CMMotionManager()
     var timer: Timer!
 
     func myAccelerometer(){
         motionManager.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
-        motionManager.startAccelerometerUpdates() //start it without a handler
-        
-        if let data = motionManager.accelerometerData {
-            let x = data.acceleration.x
-            let y = data.acceleration.y
-            let z = data.acceleration.z
-            // Use the accelerometer data
-            self.xAccel.text = "x: \x"
-            self.yAccel.text = "y: \y"
-            self.zAccel.text = "z: \z"
-        }//end if
-        
-        
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {
+            deviceManager, error in
+            let accelerationThreshold:Double = 0.8
+            let userAcceleration:CMAcceleration = (deviceManager?.userAcceleration)!
+            if (fabs(userAcceleration.y) > accelerationThreshold){
+                print("Low pass filter succeeded")
+            }
+        })
+        motionManager.startAccelerometerUpdates(to:OperationQueue.current!){(data, error) in   //start it without a handler
+        print(data as Any)
+            if let data = self.motionManager.accelerometerData {
+                self.view.reloadInputViews()
+                let x = data.acceleration.x
+                let y = data.acceleration.y
+                let z = data.acceleration.z
+                // Use the accelerometer data
+                self.xAccel!.text = "x: \(Double(x).rounded(toPlaces:3))"
+                self.yAccel!.text = "y: \(Double(y).rounded(toPlaces:3))"
+                self.zAccel!.text = "z: \(Double(z).rounded(toPlaces:3))"
+            }//end if
+        }
     }//end func
-    
-    
-    
-    
 }
-
+extension Double{
+    func rounded(toPlaces places:Int)->Double{
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
